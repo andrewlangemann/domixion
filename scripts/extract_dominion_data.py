9721c1bdec89353deff81eda8da7aeb89b48129c
Curtis den Otter <curtis.denotter@gmail.com>
2017-09-02 00:58:55 -0600

    Parsing set name, creating data structure, guessing image URL

--- scripts/extract_dominion_data.py
+++ scripts/extract_dominion_data.py
@@ -1,4 +1,5 @@
 import json
+import re
 import requests
 import urlparse
 from bs4 import BeautifulSoup
@@ -8,7 +9,7 @@ base_url = r'http://dominion.diehrstraits.com/?set=All&f=list'
 
 
 def ExtractData():
-    data = {}
+    data = {'sets': []}
 
     with open('dominion-data.html') as f:
         soup = BeautifulSoup(f.read())
@@ -16,16 +17,28 @@ def ExtractData():
     headings = soup.find_all('h2')
 
     for heading in headings:
+        setName = re.match(r'Dominion: (.+)', heading.string).group(1)
+        setData = {'name': setName, 'cards': []}
+
         table = heading.find_next_sibling('table')
         for row_index, tr in enumerate(table.find_all('tr')):
             # Skip header row
             if row_index == 0:
                 continue
 
+            card = {}
+
             cardNameCell = tr.find(class_='card-name')
-            cardUrl = urlparse.urljoin(base_url, cardNameCell.find('a'))
-            cardName = cardNameCell.string
-            cardNumber = tr.find(class_='card-number').string
+            card['card-url'] = urlparse.urljoin(base_url, cardNameCell.find('a'))
+            card['name'] = cardNameCell.string
+            card['number'] = tr.find(class_='card-number').string
+            card['cost'] = tr.find(class_='card-cost').string
+            card['card-type'] = tr.find(class_='card-type').string
+            card['rules'] = tr.find(class='card-rules').string
+            # Guessing at image url
+            imagePath = 'scans/{0}/{1}.jpg'.format(
+                    lower(setName), lower(card['name']))
+            card['image-url'] = urlparse.urljoin(base_url, imagePath)
 
 
     return data
